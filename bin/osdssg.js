d63c322da2c8ce8a8383d39aeeb3488e70cbcce2
Hung <69558780+nguyenhung15913@users.noreply.github.com>
2021-09-30 16:50:12 -0400

    adding -l flag for issue--1

--- bin/osdssg.js
+++ bin/osdssg.js
@@ -1,199 +1,218 @@
 #!/usr/bin/env node
 
-const yargs = require('yargs')
-const fs = require('fs')
-const path = require('path')
+const yargs = require("yargs");
+const fs = require("fs");
+const path = require("path");
 
-const parse = require('node-html-parser').parse
+const parse = require("node-html-parser").parse;
 
 const textToP = (input) => {
 	return input
 		.split(/\r?\n/)
 		.map((elem) => `<p>${elem}</p>`)
-		.join('\n')
-}
+		.join("\n");
+};
 const textToPMd = (input) => {
 	return input
 		.split(/[\r?\n\r?\n]/g)
 		.map((line) => {
 			return line
-				.replace(/^# (.*$)/gim, '<h1>$1</h1>')
-				.replace(/^## (.*$)/gim, '<h2>$1</h2>')
-				.replace(/^### (.*$)/gim, '<h3>$1</h3>')
-				.replace(/^#### (.*$)/gim, '<h4>$1</h4>')
-				.replace(/(^[a-z](.*)$)/gim, '<p>$1</p>')
-				.replace(/^\> (.*$)/gim, '<blockquote>$1</blockquote>')
-				.replace(/\*\*(.*?!*)\*\*/gim, '<strong> $1 </strong>')
-				.replace(/\*(.*?!*)\*/gim, '<i> $1 </i>')
+				.replace(/^# (.*$)/gim, "<h1>$1</h1>")
+				.replace(/^## (.*$)/gim, "<h2>$1</h2>")
+				.replace(/^### (.*$)/gim, "<h3>$1</h3>")
+				.replace(/^#### (.*$)/gim, "<h4>$1</h4>")
+				.replace(/(^[a-z](.*)$)/gim, "<p>$1</p>")
+				.replace(/^\> (.*$)/gim, "<blockquote>$1</blockquote>")
+				.replace(/\*\*(.*?!*)\*\*/gim, "<strong> $1 </strong>")
+				.replace(/\*(.*?!*)\*/gim, "<i> $1 </i>");
 		})
-		.join('\n')
-}
+		.join("\n");
+};
 
 const getTitle = (input) => {
-	return input.split(/\r?\n/).slice(0, 1)
-}
+	return input.split(/\r?\n/).slice(0, 1);
+};
 
 const takeFile = () => {
-	return yargs.argv._[0]
-}
+	return yargs.argv._[0];
+};
 
-let cli = require('yargs')
-const { string } = require('yargs')
+const addingStyleSheet = (parsedHtml) => {
+	if (command.s) {
+		let head = parsedHtml.querySelector("head");
+		head.appendChild(parse(`<link href="${command.s}" rel="stylesheet" />`));
+	}
+};
+
+const updatingLang = (parsedHtml) => {
+	if (command.l) {
+		if (command.l.length > 0 && command.l.length !== undefined) {
+			parsedHtml.childNodes[1].rawAttrs = `lang="${command.l}"`;
+			return console.log(`Language has been set`);
+		}
+		return console.log("Please add chosen language after flag!");
+	}
+};
+
+let cli = require("yargs");
+const { string } = require("yargs");
 
 cli = cli
-	.usage('All available options for OSDSSG: ')
+	.usage("All available options for OSDSSG: ")
 	.version(
 		true,
-		'Display current version of osdssg',
-		'Current version of osdssg is 1.0.0'
+		"Display current version of osdssg",
+		"Current version of osdssg is 1.0.0"
 	)
-	.option('help', {
-		alias: 'h',
-		desc: 'Displaying all options available'
+	.option("help", {
+		alias: "h",
+		desc: "Displaying all options available"
 	})
-	.option('input', {
-		type: 'boolean',
-		alias: 'i',
+	.option("input", {
+		type: "boolean",
+		alias: "i",
 		desc: "Input file's name or directory's name e.g: osdssg --input text.txt or osdssg -i ./dir1"
 	})
-	.option('version', {
-		alias: 'v',
-		desc: 'Show current version of osdssg'
+	.option("version", {
+		alias: "v",
+		desc: "Show current version of osdssg"
 	})
-	.option('stylesheet', {
-		alias: 's',
-		desc: 'Optionally specify a URL to a CSS stylesheet in the <head> of the HTML file'
-	}).argv
+	.option("stylesheet", {
+		alias: "s",
+		desc: "Optionally specify a URL to a CSS stylesheet in the <head> of the HTML file"
+	})
+	.option("lang", {
+		alias: "l",
+		desc: "Add an option flag to indicates the language of the html element. e.g: --lang fr is lang='fr' means using French"
+	}).argv;
 
-const command = yargs.argv
+const command = yargs.argv;
 
 if (command.i || command.input) {
 	if (command._.length <= 0) {
-		return console.log('Please enter file name or folder e.g: --input text.txt')
+		return console.log(
+			"Please enter file name or folder e.g: --input text.txt"
+		);
 	}
 
 	fs.readFile(
-		path.join(__dirname, 'htmlTemplate.html'),
-		'utf-8',
+		path.join(__dirname, "htmlTemplate.html"),
+		"utf-8",
 		(err, html) => {
-			if (err) return console.log(err)
+			if (err) return console.log(err);
 
-			const fileOrDir = takeFile()
+			const fileOrDir = takeFile();
 
 			if (!fs.existsSync(fileOrDir)) {
-				return console.log('No File or Directory Found')
+				return console.log("No File or Directory Found");
 			}
 
-			const stats = fs.statSync(fileOrDir)
+			const stats = fs.statSync(fileOrDir);
 
-			const dir = path.join(process.cwd(), 'dist')
+			const dir = path.join(process.cwd(), "dist");
 
 			if (!fs.existsSync(dir)) {
-				fs.mkdirSync(dir)
+				fs.mkdirSync(dir);
 			} else {
-				fs.rmdirSync(dir, { recursive: true })
-				fs.mkdirSync(dir)
+				fs.rmdirSync(dir, { recursive: true });
+				fs.mkdirSync(dir);
 			}
 
 			if (stats.isFile()) {
-				if (!fileOrDir.includes('.txt') && !fileOrDir.includes('.md')) {
+				if (!fileOrDir.includes(".txt") && !fileOrDir.includes(".md")) {
 					return console.log(
-						'Only .txt files and .md files can be supported in this tool!'
-					)
+						"Only .txt files and .md files can be supported in this tool!"
+					);
 				}
-				const isMDfile = fileOrDir.endsWith('.md')
-				fs.readFile(`${fileOrDir}`, 'utf-8', (error, data) => {
-					if (error) return console.log(error)
 
-					let parsedHtml = parse(html)
-					let title
-					let bodyPart
-					let body = parsedHtml.querySelector('body')
-					if (command.s) {
-						let head = parsedHtml.querySelector('head')
-						head.appendChild(
-							parse(`<link href="${command.s}" rel="stylesheet" />`)
-						)
-					}
+				const isMDfile = fileOrDir.endsWith(".md");
+				fs.readFile(`${fileOrDir}`, "utf-8", (error, data) => {
+					if (error) return console.log(error);
+
+					let parsedHtml = parse(html);
+
+					addingStyleSheet(parsedHtml);
+					updatingLang(parsedHtml);
+
+					let title;
+					let bodyPart;
+					let body = parsedHtml.querySelector("body");
 
 					if (isMDfile) {
 						// if it is md file, the title will be the first h1
-						bodyPart = textToPMd(data)
-						body.appendChild(parse(bodyPart))
-						title = body.querySelector('h1')?.text || 'Document'
+						bodyPart = textToPMd(data);
+						body.appendChild(parse(bodyPart));
+						title = body.querySelector("h1")?.text || "Document";
 					} else {
-						title = getTitle(data)
-						bodyPart = textToP(data)
-						body.appendChild(parse(bodyPart))
-						body.querySelector('p').replaceWith(parse(`<h1>${title}</h1>`))
+						title = getTitle(data);
+						bodyPart = textToP(data);
+						body.appendChild(parse(bodyPart));
+						body.querySelector("p").replaceWith(parse(`<h1>${title}</h1>`));
 					}
-					parsedHtml.querySelector('title').set_content(title)
+					parsedHtml.querySelector("title").set_content(title);
 					if (
 						!fs.existsSync(
-							path.join(process.cwd(), 'dist', `${fileOrDir}.html`)
+							path.join(process.cwd(), "dist", `${fileOrDir}.html`)
 						)
 					) {
 						fs.writeFile(
 							`${process.cwd()}/dist/index.html`,
 							parsedHtml.toString(),
 							(e) => {
-								if (e) return console.log(e)
-								console.log('New file has been created!!')
+								if (e) return console.log(e);
+								console.log("New file has been created!!");
 							}
-						)
+						);
 					}
-				})
+				});
 			} else {
-				const files = fs.readdirSync(fileOrDir)
+				const files = fs.readdirSync(fileOrDir);
 				if (files.length <= 0) {
-					return console.log('There is no file in the directory')
+					return console.log("There is no file in the directory");
 				}
-				let count = 0
+				let count = 0;
 				files.forEach((file) => {
-					const isMDfile = file.endsWith('.md')
-					fs.readFile(path.join(fileOrDir, file), 'utf-8', (error, data) => {
-						if (error) return console.log(error)
-						let parsedHtml = parse(html)
+					const isMDfile = file.endsWith(".md");
+					fs.readFile(path.join(fileOrDir, file), "utf-8", (error, data) => {
+						if (error) return console.log(error);
+						let parsedHtml = parse(html);
 
-						if (command.s) {
-							let head = parsedHtml.querySelector('head')
-							head.appendChild(
-								parse(`<link href="${command.s}" rel="stylesheet" />`)
-							)
-						}
-						let title
-						let bodyPart
-						let body = parsedHtml.querySelector('body')
+						addingStyleSheet(parsedHtml);
+						updatingLang(parsedHtml);
+
+						let title;
+						let bodyPart;
+						let body = parsedHtml.querySelector("body");
 						if (isMDfile) {
 							// if it is md file, the title will be the first h1
-							bodyPart = textToPMd(data)
-							body.appendChild(parse(bodyPart))
-							title = body.querySelector('h1')?.text || 'Document'
+							bodyPart = textToPMd(data);
+							body.appendChild(parse(bodyPart));
+							title = body.querySelector("h1")?.text || "Document";
 						} else {
-							title = getTitle(data)
-							bodyPart = textToP(data)
-							body.appendChild(parse(bodyPart))
-							body.querySelector('p').replaceWith(parse(`<h1>${title}</h1>`))
+							title = getTitle(data);
+							bodyPart = textToP(data);
+							body.appendChild(parse(bodyPart));
+							body.querySelector("p").replaceWith(parse(`<h1>${title}</h1>`));
 						}
 
-						parsedHtml.querySelector('title').set_content(title)
-						const pathName = `${process.cwd()}/dist/${file}.html`
+						parsedHtml.querySelector("title").set_content(title);
+						const pathName = `${process.cwd()}/dist/${file}.html`;
 
 						if (!fs.existsSync(pathName)) {
-							count++
+							count++;
 
 							fs.writeFile(
-								path.join(process.cwd(), 'dist', `index${count}.html`),
+								path.join(process.cwd(), "dist", `index${count}.html`),
 								parsedHtml.toString(),
 								(e) => {
-									if (e) return console.log(e)
-									console.log('New file has been created in dist directory!!')
+									if (e) return console.log(e);
+									console.log("New file has been created in dist directory!!");
 								}
-							)
+							);
 						}
-					})
-				})
+					});
+				});
 			}
 		}
-	)
+	);
 }
